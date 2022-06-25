4754aa3a72f10a4b3c7d27fb23b8072a22953394
Machy <Machy@TheGatewayToANewWorld.com>
2022-06-25 11:54:03 +0100

    Readme update. Adding speed limits to config

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # dotRPFunctions
-This is a collection of usefull FiveM Functions we are using in our server, as configurable as we can make stuff
+
+This is a collection of useful FiveM Functions we are using in our server, as configurable as we can make stuff
+
+
+
+
+--------------------------------------------------------------------------
+Code Commenting:
+***Top of code block***
+This will be in the files just above the config changes
+**--FILE** "Shows you that the file this config section effects is in the Client folder and is called "Traffic""
+--FILE
+--client/Traffic.Lua
+**--Desc**#Bellow this will be a short 1 line description of what this does
+--Desc
+--This is a line in the readme
+**--IMPORTANT** Something you MUST READ before modifying
+--IMPORTANT
+--This will tell you about stuff that you need to know
+
+**--Suggestion** This is normally what we have found worked well on our server
+--Suggestion
+--Doing this might also work well for you
+
+**--INFO** This will be some information that just makes no sense to but inline, and is more FYI Than must read
+--INFO
+--FiveM Docs are painful but useful <https://docs.fivem.net/docs/>
+
+***Inline Comments***
+**Natives**
+~:Sets "We use this in the config file to denote what native is set where applicable"
+
+**Must be**
+Must be a float! -- This means the number must have a decimal place, IE 1.0
+Must be a INT! -- This means the number must be a whole number!
--- config.lua
+++ config.lua
@@ -4,6 +4,11 @@
 Config = {} --Inits stuff
 ---------------------------------------------------
 --Traffic and Ped Levels
+--FILE
+--client/Traffic.lua
+
+--Desc
+--Sets how much life is in the city
     Config.Foot_Peds_Multiplier = 10.0 --This is the Multplier for how many peds that walk around exsist! Must be a float! ~:Sets https://runtime.fivem.net/doc/natives/#_0x7A556143A1C03898
     Config.Road_Vech_Multiplier = 10.0 --This is the Multipler for how many cars will be on the road! Must be a float! https://docs.fivem.net/natives/?_0x245A6883D966D537
     Config.Rand_Vech_Multiplier = 10.0 --Sets Randomness of cars! Must be a float! ~:Sets https://runtime.fivem.net/doc/natives/#_0xB3B3359379FE77D3
@@ -15,14 +20,52 @@ Config = {} --Inits stuff
 --It seems to be effected by the sever power though
 -------------------------------------------
 --Speed Limits
+--FILE client/Speed.lua
+
+--Desc
+--The sets the speedlimits for vechs
+
+--IMPORTANT
+--this will overide other limiters (Like the one in JOEHUD) Set to -1 To disable for that vech
+--this is based of the Vech class in <vehicleClass>xxxx</vehicleClass> in vehicles.meta, it does not care what is in your database!
+
+--Suggestion
+--180 seems like a good max speed so far in testing, with 250 for heli/ac. You really dont want to be going much higher than this
+
+--Info
+--Car Class Info https://docs.fivem.net/natives/?_0x29439776AAA00A62
+Config.Class_Compacts_Maxspeed = 120 --Class 0
+Config.Class_Sedan_Maxspeed = 120 --Class 1
+Config.Class_SUV_Maxspeed = 110 --Class 2
+Config.Class_Coupes_Maxspeed = 130 --Class 3
+Config.Class_Muscle_Maxspeed = 160 --Class 4
+Config.Class_SportsClassic_Maxspeed = 170 --Class 5
+Config.Class_Sports_Maxspeed = 180 --Class 6
+Config.Class_Super_Maxspeed = 190 --Class 7
+Config.Class_Motorcycle_Maxspeed = 150 --Class 8
+Config.Class_Offroad_Maxspeed = 120 --Class 9
+Config.Class_Industrial_Maxspeed = 80 --Class 10
+Config.Class_Utility_Maxspeed = 80 --Class 11
+Config.Class_Vans_Maxspeed = 120 --Class 12
+Config.Class_Cycles_Maxspeed = 30 --Class 13
+Config.Class_Boats_Maxspeed = 80 --Class 14
+Config.Class_Helicopters_Maxspeed = -1 --300 --Class 15
+Config.Class_Planes_Maxspeed = -1 --300 --Class 16
+Config.Class_Service_Maxspeed = 120 --Class 17
+Config.Class_Emergency_Maxspeed = -1 --Class 18
+Config.Class_Military_Maxspeed = 120 --Class 19
+Config.Class_Commercial_Maxspeed = 120 --Class 20
+
+
+
 
 
 
 ----------------------------------------------
 --Trains
-Config.Freight_Trains = true --Should Freight Trains Exsist! true/false must be in lowercase
-Config.Tram_Trains = false --Should Trams Exsist! true/false must be in lowercase
-Config.Train_Spawn_Freq = 100000 --How Often should trains spawn, Smaller number more trains 20000 is lowest you wanna go
-Config.Train_Count = 10 --How Many trains should exist (kinda) 10 is a good number
-Config.Train_Disable_Doors = false --Should doors be disabled on trams, they are nicely ridable in our working
+Config.Freight_Trains = true --Should Freight Trains Exsist! true/false must be in lowercase  --Im not 100% sure how accurate the docs are on this one! ~:Sets https://docs.fivem.net/natives/?_0xFD813BB7DB977F20
+Config.Tram_Trains = true --Should Trams Exsist! true/false must be in lowercase ~:Sets https://docs.fivem.net/natives/?_0xFD813BB7DB977F20
+Config.Train_Spawn_Freq = 100000 --How Often should trains spawn, Smaller number more trains 20000 is lowest you wanna go ~:Sets https://docs.fivem.net/natives/?_0x21973BBF8D17EDFA
+Config.Train_Count = 10 --How Many trains should exist (kinda) 10 is a good number ~:Sets https://docs.fivem.net/natives/?_0x80D9F74197EA47D9
+Config.Train_Disable_Doors = false --Should doors be disabled on trams, they are nicely ridable in our working ~:Sets https://docs.fivem.net/natives/?_0xD4D1BA63
 ---------------------------------------------
